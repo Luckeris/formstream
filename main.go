dd7127b168f21f66ce54388049f208c41794fc48
﻿Luckeris <tamten.jakub1@gmail.com>
2026-08-01 18:47:56 +0000

    Added a form structure and integrated it into the POST endpoint + added a README.md and LICENSE

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type FormSubmission struct {
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Message string `json:"message"`
+}
+
 func submit(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Endpoint pro formulář")
+	var data FormSubmission
+
+	if r.Method != http.MethodPost {
+		fmt.Fprintln(w, "Only POST method is allowed.")
+
+		return
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+
+		return
+	}
+
+	fmt.Printf("Received from form -> Name: %s, Email: %s, Message: %s\n", data.Name, data.Email, data.Message)
+	fmt.Fprintln(w, "Form was succesfully received.")
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +41,6 @@ func main() {
 	http.HandleFunc("/submit", submit)
 	http.HandleFunc("/", home)
 
-	fmt.Println("Server běží na http://localhost:8080")
+	fmt.Println("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
