fa4673b0e5e96596e999e5bb2d871289e508f637
﻿Luckeris <tamten.jakub1@gmail.com>
2026-08-03 20:02:02 +0200

    Added the Discord Webhook functionality, that we intended, now if you setup your webhookURL via the tutorial your form submittions will get sent to your discord

--- main.go
+++ main.go
@@ -1,27 +1,51 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
 )
 
+// Structure of FormSubmission Message
 type FormSubmission struct {
 	Name    string `json:"name"`
 	Email   string `json:"email"`
 	Message string `json:"message"`
 }
 
+// Structure of a Discord Message
+type DiscordMessage struct {
+	Content string `json:"content"`
+}
+
+// Function that parses the data from FormSubmission into the Content of the Discord Message, converts into JsonBytes and sends it via http.Post on your webhookURL
+func sendToDiscord(webhookURL string, data FormSubmission) {
+	var msg DiscordMessage
+	msg.Content = fmt.Sprintf("Name: %s , Message: %s , Email: %s", data.Name, data.Message, data.Email)
+	jsonBytes, err := json.Marshal(msg)
+	if err != nil {
+		return
+	}
+	http.Post(webhookURL, "application/json", bytes.NewBuffer(jsonBytes))
+
+}
+
+// Function that handles all submiting information of form, verifies the actual correct information format so the app doesnt crash.
 func submit(w http.ResponseWriter, r *http.Request) {
 	var data FormSubmission
+	webhookURL := os.Getenv("DISCORD_WEBHOOK_URL")
 
+	//If a user tries to call different method than POST
 	if r.Method != http.MethodPost {
 		fmt.Fprintln(w, "Only POST method is allowed.")
 
 		return
 	}
 
+	//Here we decode data and if the payload is incorrect, it gives us an error
 	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
@@ -29,6 +53,7 @@ func submit(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//In the functions below we check for the correct formatting of the data, we also check if the data is not empty
 	if strings.TrimSpace(data.Name) == "" {
 		http.Error(w, "Name cannot be empty", http.StatusBadRequest)
 		return
@@ -45,9 +70,15 @@ func submit(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Printf("Received from form -> Name: %s, Email: %s, Message: %s\n", data.Name, data.Email, data.Message)
+
+	//If the webhookURL isnt empty, we send the data to the webhook, if it is, we dont
+	if strings.TrimSpace(webhookURL) != "" {
+		sendToDiscord(webhookURL, data)
+	}
 	fmt.Fprintln(w, "Form was succesfully received.")
 }
 
+// Default HOME Endpoint
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "FormStream API Server")
 }
